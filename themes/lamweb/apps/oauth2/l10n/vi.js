dc3224a43d151d7c9c02a5e3be2ec4353385f6d1
huynhvothinh <huynhvothinh@gmail.com>
2018-05-08 17:49:40 +0700

    translate

--- themes/lamweb/apps/oauth2/l10n/vi.js
+++ themes/lamweb/apps/oauth2/l10n/vi.js
@@ -1,14 +1,14 @@
+OC.L10N.register(
+   "core",
 {
-    "translations": {
-        "OAuth 2.0": "OAuth 2.0 ",
-        "OAuth 2.0 clients": "kết nối OAuth 2.0",
-        "OAuth 2.0 allows external services to request access to %s.": "OAuth 2.0 cho phép các dịch vụ bên ngoài yêu cầu quyền truy cập vào% s. ",
-        "Name": "Tên",
-        "Redirection URI": "Liên kết chuyển tiếp",
-        "Client Identifier": "Mã định danh khách hàng ",
-        "Secret": "Mật khẩu",
-        "Add client": "Thêm kết nối",
-        "Add": "Thêm"
-    },
-    "pluralForm": "nplurals=1; plural=0;"
-}
+    "OAuth 2.0": "OAuth 2.0 ",
+    "OAuth 2.0 clients": "kết nối OAuth 2.0",
+    "OAuth 2.0 allows external services to request access to %s.": "OAuth 2.0 cho phép các dịch vụ bên ngoài yêu cầu quyền truy cập vào% s. ",
+    "Name": "Tên",
+    "Redirection URI": "Liên kết chuyển tiếp",
+    "Client Identifier": "Mã định danh khách hàng ",
+    "Secret": "Mật khẩu",
+    "Add client": "Thêm kết nối",
+    "Add": "Thêm"
+}   ,
+"nplurals=1; plural=0;");
