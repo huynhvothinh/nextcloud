9e871c0ad0acf4bc753a7d70751f6c774871b7b1
huynhvothinh <huynhvothinh@gmail.com>
2018-05-08 12:33:34 +0700

    UPDATE translate

--- themes/lamweb/apps/files_versions/l10n/vi.js
+++ themes/lamweb/apps/files_versions/l10n/vi.js
@@ -1,12 +1,12 @@
+OC.L10N.register(
+   "core",
 {
-    "translations": {
-        "Could not revert: %s": "Không thể khôi phục: %s",
-        "Versions": "Phiên bản",
-        "Failed to revert {file} to revision {timestamp}.": "Thất bại khi trở lại {file} khi sử đổi {timestamp}.",
-        "_%n byte_::_%n bytes_": "_% n byte _ :: _% n byte _ ",
-        "Restore": "Khôi phục",
-        "No earlier versions available": "Không có phiên bản nào trước đó có sẵn ",
-        "More versions …": "Các phiên bản khác… "
-    },
-    "pluralForm": "nplurals=1; plural=0;"
-}
+    "Could not revert: %s": "Không thể khôi phục: %s",
+    "Versions": "Phiên bản",
+    "Failed to revert {file} to revision {timestamp}.": "Thất bại khi trở lại {file} khi sử đổi {timestamp}.",
+    "_%n byte_::_%n bytes_": "_% n byte _ :: _% n byte _ ",
+    "Restore": "Khôi phục",
+    "No earlier versions available": "Không có phiên bản nào trước đó có sẵn ",
+    "More versions …": "Các phiên bản khác… "
+}   ,
+"nplurals=1; plural=0;");
