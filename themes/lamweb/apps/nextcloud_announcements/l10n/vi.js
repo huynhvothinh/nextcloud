f2c4bf79a7336e246c54484a5b4f5f5104f08a49
huynhvothinh <huynhvothinh@gmail.com>
2018-05-08 12:30:01 +0700

    update translate

--- themes/lamweb/apps/nextcloud_announcements/l10n/vi.js
+++ themes/lamweb/apps/nextcloud_announcements/l10n/vi.js
@@ -1,9 +1,9 @@
-OC.L10N.register(
-   "core",
 {
-    "Nextcloud announcements": "Thông báo Nextcloud",
-    "Nextcloud announcement": "Thông báo Nextcloud",
-    "Shows the latest news of Nextcloud in your notifications.": "Hiển thị tin tức mới nhất của Nextcloud trong thông báo của bạn.",
-    "These groups will be notified about the announcements.": "Những nhóm này sẽ được biết về các thông báo."
-}   ,
-"nplurals=1; plural=0;");
+    "translations": {
+        "Nextcloud announcements": "Thông báo Nextcloud",
+        "Nextcloud announcement": "Thông báo Nextcloud",
+        "Shows the latest news of Nextcloud in your notifications.": "Hiển thị tin tức mới nhất của Nextcloud trong thông báo của bạn.",
+        "These groups will be notified about the announcements.": "Những nhóm này sẽ được biết về các thông báo."
+    },
+    "pluralForm": "nplurals=1; plural=0;"
+}
