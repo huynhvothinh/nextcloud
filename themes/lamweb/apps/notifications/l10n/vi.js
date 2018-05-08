3d4485a39c4149fd5feb5d961b6c4780bfa12b48
huynhvothinh <huynhvothinh@gmail.com>
2018-05-08 11:48:10 +0700

    update translate

--- themes/lamweb/apps/notifications/l10n/vi.js
+++ themes/lamweb/apps/notifications/l10n/vi.js
@@ -1,9 +1,13 @@
 OC.L10N.register(
-    "notifications",
-    {
-    "in {path}" : "trong {path}",
-    "Notifications" : "Thông báo",
-    "No notifications" : "Không thông báo ",
-    "Dismiss" : "Bỏ qua"
-},
+   "core",
+{
+    "Failed to perform action": "Không thể thực hiện hành động ",
+    "Failed to dismiss notification": "Không thể loại bỏ thông báo ",
+    "Failed to dismiss all notifications": "Không thể loại bỏ tất cả thông báo ",
+    "in {path}": "trong {path}",
+    "Admin notifications": "Thông báo quản trị ",
+    "Notifications": "Thông báo",
+    "No notifications": "Không thông báo ",
+    "Dismiss": "Bỏ qua"
+}   ,
 "nplurals=1; plural=0;");
